ba74326eec378e61a1f1845c0be024eba1ffba99
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-23 10:55:02 -0600

    add login comp

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import './authDetails.js'
+import { credentials } from './authDetails.js';
+
+function Login() {
+
+  const client_id = credentials.client_id;
+  const authEndpoint = "https://accounts.spotify.com/authorize"
+
+  return (
+    <div className="Login">
+     
+    </div>
+  );
+}
+
+export default Login;
