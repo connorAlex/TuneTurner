227c1922b26cee6d16e4f4366bf54fef0255c620
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-23 13:42:43 -0600

    set up implicit OAuth

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,26 @@
-import './authDetails.js'
-import { credentials } from './authDetails.js';
+import React from 'react';
 
 function Login() {
 
-  const client_id = credentials.client_id;
-  const authEndpoint = "https://accounts.spotify.com/authorize"
+  const client_id = '63b963509ce94361be2659a9a6328ed3';
+  const redirect_uri = 'http://localhost:3002/callback';
+  const scope = 'user-read-private user-read-email';
+
+
+  let url = "https://accounts.spotify.com/authorize";
+  url += "?response_type=token";
+  url += `&client_id=${encodeURIComponent(client_id)}`;
+  url += `&scope=${encodeURIComponent(scope)}`;
+  url += `&redirect_uri=${encodeURIComponent(redirect_uri)}`;
+  //url += `&state=${encodeURIComponent(state)}`;
+
+  const handleClick = () => {
+    window.open(url);
+  }
 
   return (
     <div className="Login">
-     
+        <button onClick={handleClick}>Login to Spotify</button>
     </div>
   );
 }
