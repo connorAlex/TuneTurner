07f7c5871fb123f8c49547e4a15b8ca91a98a496
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-23 15:40:04 -0600

    getUser funct

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,22 @@ const Home = () => {
 
     
     const hash = new URLSearchParams(window.location.hash.substring(1)).get("access_token");
-    console.log(hash);
+    const base = 'https://api.spotify.com/v1';
+
+    const getUser = async () => {
+        const info = await fetch(base + "/me", {
+            method: 'GET',
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + hash,
+            }
+        });
+
+        return info.json();
+
+    }
+    console.log(getUser());
 
     return (
         <div className='Home'>
