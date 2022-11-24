bc00e86602bb901def747b03e4b8edaf6d1c8f97
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-23 19:50:52 -0600

    prettier reformat

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,48 +1,46 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const Home = () => {
-    const [user, setUser] = useState();
-    const [playlists, setPlaylists] = useState();
-    
-    const hash = new URLSearchParams(window.location.hash.substring(1)).get("access_token");
-    const base = 'https://api.spotify.com/v1';
+  const [user, setUser] = useState();
+  const [playlists, setPlaylists] = useState();
 
-    useEffect(() => {
-        const setState = async () => {
-           const user = await callSpotify("/me"); 
-           setUser(await user);
-            setPlaylists(await callSpotify(`/users/${user.id}/playlists`))
-        }
-        setState();
-        
-    }, []);
+  const hash = new URLSearchParams(window.location.hash.substring(1)).get(
+    "access_token"
+  );
+  const base = "https://api.spotify.com/v1";
 
-
-    const callSpotify = async (endpoint) => {
-        let response = await fetch(base + endpoint, {
-            method: 'GET',
-            mode: 'cors',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + hash,
-            }
-        });
-
-        const data = await response.json();
-        console.log(data);
-        return data;
+  useEffect(() => {
+    const setState = async () => {
+      const user = await callSpotify("/me");
+      setUser(await user);
+      setPlaylists(await callSpotify(`/users/${user.id}/playlists`));
     };
+    setState();
+  }, []);
 
-    return (
-        <div className='Home'>
-            <SkeletonTheme width="95%" baseColor="#f3f4f6" highlightColor="#d1d5db">
-                <div>
-                    {user ? (<div>{user.display_name}</div>) : (<Skeleton/>)}
-                </div>
-            </SkeletonTheme>
-        </div>  
-    );
-}
+  const callSpotify = async (endpoint) => {
+    let response = await fetch(base + endpoint, {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + hash,
+      },
+    });
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+  };
+
+  return (
+    <div className="Home">
+      <SkeletonTheme width="95%" baseColor="#f3f4f6" highlightColor="#d1d5db">
+        <div>{user ? <div>{user.display_name}</div> : <Skeleton />}</div>
+      </SkeletonTheme>
+    </div>
+  );
+};
 
 export default Home;
