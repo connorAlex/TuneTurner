1abb4bef49ae854fde9ebbf2c0a8f4733ae8666d
connorAlex <alex.connor.cochrane@gmail.com>
2022-11-23 19:50:26 -0600

    create universal API call function + playlist call

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 const Home = () => {
     const [user, setUser] = useState();
+    const [playlists, setPlaylists] = useState();
     
     const hash = new URLSearchParams(window.location.hash.substring(1)).get("access_token");
     const base = 'https://api.spotify.com/v1';
 
-    useEffect( () => {
-        getUser();
+    useEffect(() => {
+        const setState = async () => {
+           const user = await callSpotify("/me"); 
+           setUser(await user);
+            setPlaylists(await callSpotify(`/users/${user.id}/playlists`))
+        }
+        setState();
+        
     }, []);
 
-    const getUser = async () => {
-        let response = await fetch(base + "/me", {
+
+    const callSpotify = async (endpoint) => {
+        let response = await fetch(base + endpoint, {
             method: 'GET',
             mode: 'cors',
             headers: {
@@ -21,13 +30,17 @@ const Home = () => {
         });
 
         const data = await response.json();
-        setUser(await data);
-    }
-    
+        console.log(data);
+        return data;
+    };
 
     return (
         <div className='Home'>
-            this is home {user.country}
+            <SkeletonTheme width="95%" baseColor="#f3f4f6" highlightColor="#d1d5db">
+                <div>
+                    {user ? (<div>{user.display_name}</div>) : (<Skeleton/>)}
+                </div>
+            </SkeletonTheme>
         </div>  
     );
 }
