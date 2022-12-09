f214d378effeaf33952dbd90460edd4abb7ae6de
connorAlex <alex.connor.cochrane@gmail.com>
2022-12-09 10:37:45 -0600

    add searchSong call on click

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import "../styles/Track.css";
 
-const Track = ({track}) => {
-
+const Track = ({track, searchSong, info, asins}) => {
+    //https://www.amazon.com/gp/aws/cart/add.html?ASIN.1=[ASIN]
     const msConvert = (ms) => {
         let totalSeconds = ms / 1000
         let minutes = Math.floor(totalSeconds / 60); 
@@ -15,10 +15,14 @@ const Track = ({track}) => {
         return (`${minutes}:${seconds}`);
     }
 
+    const handleClick = async (e) => {
+        await searchSong(info);
+    }
+
     return (
-        <div className='Track'>
+        <div className='Track' onClick={handleClick} >
             {track.name}
-            <div>
+            <div >
                 <div>{msConvert(track.duration_ms)}</div>
                 <div>{track.artists[0].name}</div>
                 <div>{track.album.name}</div>
