98da473025b0b21fd36fb0be9e1c94518dad2563
connorAlex <alex.connor.cochrane@gmail.com>
2022-12-18 22:04:09 -0600

    add checkout comp

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../styles/Nav.css';
+import Checkout from './Checkout';
 
-const Nav = ({name, createLink}) => {
+const Nav = ({name, createLink, queryData}) => {
 
     return (
         <div className='Nav'>
             <div> Welcome, {name}. </div>
-            <div onClick={createLink}> Checkout </div>
+            <div>{queryData}</div>
+            <Checkout 
+                queryData={queryData}
+            />
         </div>
     );
 }
