d9d9c0ce81021d714976a5e006f1611201985732
connorAlex <alex.connor.cochrane@gmail.com>
2022-12-19 08:59:51 -0600

    add queryData state setting

--- src/components/PlaylistContainer.js
+++ src/components/PlaylistContainer.js
@@ -4,7 +4,7 @@ import uniqid from 'uniqid';
 import Track from './Track';
 import "../styles/PlaylistContainer.css"
 
-const PlaylistContainer = ({playlist, hash, queryData, setQueryData}) => {
+const PlaylistContainer = ({playlist, hash, setQueryData}) => {
 
     const [tracks, setTracks] = useState();
 
@@ -25,10 +25,17 @@ const PlaylistContainer = ({playlist, hash, queryData, setQueryData}) => {
 
 
     const handleClick = (e) => {
-        // for every track in the Track Container, search the song and add it to the state
-        const trackContainer = e.currentTarget.lastChild.lastChild;
-        console.log(trackContainer)
+        addPlaylist()
+    }
 
+    const addPlaylist = () => {
+        let tracksInfo = [];
+    
+        // Limited to 2 songs due to googleJSON daily throttle
+        for (let i = 0; i < 2; i++) {
+            tracksInfo.push(`${tracks.items[i].track.name}, ${tracks.items[i].track.artists[0].name}`)
+        }
+        setQueryData((currentData) => [...currentData, ...tracksInfo])
     }
 
     return (
@@ -42,6 +49,7 @@ const PlaylistContainer = ({playlist, hash, queryData, setQueryData}) => {
                     <img src={playlist.images[0].url} alt = "album cover"/>
                 </div>
                 <div className='TrackContainer'>
+                    {/* Limited to 2 songs due to googleJSON daily throttle */}
                     {tracks && tracks.items.slice(0, 2).map((item) => {
                         
                         return (
@@ -49,7 +57,6 @@ const PlaylistContainer = ({playlist, hash, queryData, setQueryData}) => {
                                 key={uniqid()}
                                 track={item.track}
                                 info={`${item.track.name}, ${item.track.artists[0].name}`}
-                                queryData={queryData}
                                 setQueryData={setQueryData}
                             />
                         );
