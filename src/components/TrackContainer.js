3e227274190bc757ca4d9d8f60e2c9b55529f343
connorAlex <alex.connor.cochrane@gmail.com>
2022-12-21 11:09:49 -0600

    add track select styling

---
+++ src/components/TrackContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import uniqid from 'uniqid';
+import Track from './Track';
+import '../styles/TrackContainer.css'
+
+const TrackContainer = ({tracks,setQueryData}) => {
+    
+    return (
+        <div className='TrackContainer'>
+            {/* Limited to 2 songs due to googleJSON daily throttle */}
+            {tracks && tracks.items.slice(0, 2).map((item) => {
+                        
+                        return (
+                            <Track
+                                key={uniqid()}
+                                track={item.track}
+                                info={`${item.track.name}, ${item.track.artists[0].name}`}
+                                setQueryData={setQueryData}
+                            />
+                        );
+                    })}
+        </div>
+    );
+}
+
+export default TrackContainer;
