f791bb1ee002e7c55840488343e1b649c6253e5b
connorAlex <alex.connor.cochrane@gmail.com>
2022-12-21 10:39:57 -0600

    rename comp

---
+++ src/components/PlaylistTitle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlaylistTitle = ({title}) => {
+
+    
+    return (
+        <div className='PlaylistTitle'>
+            {title}
+        </div>
+    );
+}
+
+export default PlaylistTitle;
