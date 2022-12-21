09c9d7e7c7cad6fa8465ac25c70028cfe9f99019
connorAlex <alex.connor.cochrane@gmail.com>
2022-12-21 10:37:51 -0600

    add Track Title Component

---
+++ src/components/TrackTitle.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TrackTitle = ({title}) => {
+
+    
+    return (
+        <div className='TrackTitle'>
+            {title}
+        </div>
+    );
+}
+
+export default TrackTitle;
